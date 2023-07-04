f8b4cee5bc6a4e228d10c2b8ad899ae3c708291c
Jadesdeblessed <jadesolaemmanuel@gmail.com>
2023-07-03 20:06:14 -0400

    tera

--- main.tf
+++ main.tf
@@ -24,12 +24,13 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0e5b6b6a9f3db6db8"
   instance_type = "t2.micro"
+  subnet_id = "subnet-09a059548785d815c"
 }
 
 # Refer to the arguments section for the EIP (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip#argument-reference)
 resource "aws_eip" "elasticeip" {
   instance = aws_instance.ec2.id
-  subnet_id = "subnet-09a059548785d815c"
+ 
 }
 
 # Refer to the attribute section for the EIP (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip#attributes-reference)
