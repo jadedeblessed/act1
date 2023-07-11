8c958d9c07502c412fe8f20f7b63486423e13990
Jadesdeblessed <jadesolaemmanuel@gmail.com>
2023-07-10 21:23:57 -0400

    üpdate secret

--- main.tf
+++ main.tf
@@ -17,20 +17,19 @@ terraform {
 # Provider Block
 provider "aws" {
   region = "us-west-2"
-  //access_key = "AKIA42FD5YMYERALMYRW"
-  //secret_key = "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"
+
 }
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0e5b6b6a9f3db6db8"
   instance_type = "t2.micro"
-  subnet_id = "subnet-09a059548785d815c"
+  subnet_id     = "subnet-09a059548785d815c"
 }
 
 # Refer to the arguments section for the EIP (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip#argument-reference)
 resource "aws_eip" "elasticeip" {
   instance = aws_instance.ec2.id
- 
+
 }
 
 # Refer to the attribute section for the EIP (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip#attributes-reference)
