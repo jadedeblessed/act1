c72c8631aa66338adb1e79d1c705807bba3e0469
Jadesdeblessed <jadesolaemmanuel@gmail.com>
2023-07-10 22:24:14 -0400

    üpdate secret

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 # 3) Output the value of the public IP of the EIP.
 
 # **Terraform block**
-/*
+
 terraform {
   required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
@@ -13,7 +13,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-} */
+} 
 
 # Provider Block
 provider "aws" {
